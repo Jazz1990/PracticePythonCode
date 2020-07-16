69b9836212e1d6b785f3afada698166ef6479874
Jazz <jasmsekh@amazon.com>
2020-07-16 16:28:53 +0530

    Added setupfile

--- Age_function.py
+++
@@ -1,5 +0,0 @@
-def age():
-    name=input("Enter your name:")
-    age=int(input("Enter your age:"))
-    Age_after100yr=age+int(100)
-    print("Your age after 100 years will be {0}".format(Age_after100yr))
